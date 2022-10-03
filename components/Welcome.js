d945cd7f7ca731c171659c6fa35f0b74cd79f64a
Ian Morrill <ian@ianmorrill.com>
2022-10-03 17:30:00 -0400

    removed scroll view

--- components/Welcome.js
+++ components/Welcome.js
@@ -16,9 +16,7 @@ const styles = StyleSheet.create({
 export default function Welcome() {
     return (
         <View style={styles.container}>
-            <ScrollView>
-                <Text style={styles.text}>Welcome to React Native Workshop 2!</Text>
-            </ScrollView>
+            <Text style={styles.text}>Welcome to React Native Workshop 2!</Text>
         </View>
     );
 }
